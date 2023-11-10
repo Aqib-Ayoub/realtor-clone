4162a6fbc449980d5997a803fc60261a591c2404
Aqib-Ayoub <aqibayoub321@gmail.com>
2023-11-10 09:52:28 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+// import {Route, Router, Routes } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignUp from './pages/SignUp';
+import SignIn from "./pages/SignIn";
+import ForgotPassword from './pages/ForgotPassword'
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl bg-red-400 mt-1 ml-0">Helllo World</h1>
-    </div>
+    <>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path= '/offers' element={<Offers/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/forgot-password"element={<ForgotPassword/>}/>
+
+    </Routes>
+   </Router>
+   </>
+
   );
 }
 
